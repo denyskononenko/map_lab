ac6e53d2fcf44b122d64b7b5ba3e5faabc066147
denyskononenko <kononenkody@gmail.com>
2019-08-07 13:56:26 +0300

    Updates

---
+++ precache-manifest.c02afc4bff15dec1c20d3b082a537748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a987c3b3f5e1594b5e50a6aeed1cbf52",
+    "url": "/map_lab/index.html"
+  },
+  {
+    "revision": "9f55e972bd08c063b149",
+    "url": "/map_lab/static/css/2.6639d161.chunk.css"
+  },
+  {
+    "revision": "a093d1aef271e389a379",
+    "url": "/map_lab/static/css/main.ad9543ac.chunk.css"
+  },
+  {
+    "revision": "9f55e972bd08c063b149",
+    "url": "/map_lab/static/js/2.f2ab0cb6.chunk.js"
+  },
+  {
+    "revision": "a093d1aef271e389a379",
+    "url": "/map_lab/static/js/main.96a55d34.chunk.js"
+  },
+  {
+    "revision": "5130d3baba67705d6b0b",
+    "url": "/map_lab/static/js/runtime~main.94c9c9d4.js"
+  }
+]);
